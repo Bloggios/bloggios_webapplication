87698b5e6a5024b0f850c152d173128d6f2f6e75
Rohit <rohitparih@gmail.com>
2024-04-04 14:23:03 +0530

    Privacy Policy -> `Implementation`

--- src/container/TermsContainer/PrivacyPolicy.jsx
+++ src/container/TermsContainer/PrivacyPolicy.jsx
@@ -24,50 +24,72 @@ import BloggiosBase from "../boundries/bloggiosBase";
 import styled from "styled-components";
 import {colors} from "../../styles/Theme";
 import BgTransition from "../../component/animations/BgTransition";
-
-const data = {
-    "Heading 1": <p>This is the content for Heading 1</p>,
-    "Heading 2": <div><h3>This is the content for Heading 2</h3><p>More details here...</p></div>,
-    "Heading 3": <span>This is the content for Heading 3</span>,
-    "Heading 4": "Details for Heading 4",
-    "Heading 5": "Details for Heading 5",
-    "Heading 6": "Details for Heading 6",
-    "Heading 7": "Details for Heading 7",
-    "Heading 8": "Details for Heading 8",
-    "Heading 9": "Details for Heading 9",
-    "Heading 10": "Details for Heading 10"
-};
+import {privacyNoticeData} from "../../asset/configurations/PrivacyNoticeData";
+import IconButton from "../../component/buttons/IconButton";
+import {BiHomeAlt2} from "react-icons/bi";
+import {useNavigate} from "react-router-dom";
+import {HOME_PAGE, LANDING_PAGE} from "../../constant/pathConstants";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
 
 const PrivacyPolicy = () => {
-    const [selectedHeading, setSelectedHeading] = useState(Object.keys(data)[0]); // Initialize with the first key
+
+    const navigate = useNavigate();
+    const {width} = useWindowDimensions();
+    const [selectedHeading, setSelectedHeading] = useState(Object.keys(privacyNoticeData)[0]);
 
     const handleHeadingClick = (heading) => {
         setSelectedHeading(heading);
     };
 
     return (
-        <BloggiosBase>
             <Wrapper>
                 <MainContainer>
-                    <HeadingPart>
-                        {Object.keys(data).map((heading, i) => (
-                            <HeadingCard
-                                key={i}
-                                active={selectedHeading === heading}
-                                onClick={() => handleHeadingClick(heading)}
-                            >
-                                <span>{heading}</span>
-                            </HeadingCard>
-                        ))}
-                    </HeadingPart>
+                    {width > 600 ? (
+                        <HeadingPart>
+                            {Object.keys(privacyNoticeData).map((heading, i) => (
+                                <HeadingCard
+                                    key={i}
+                                    active={selectedHeading === heading}
+                                    onClick={() => handleHeadingClick(heading)}
+                                >
+                                    <span>{heading}</span>
+                                </HeadingCard>
+                            ))}
+                        </HeadingPart>
+                    ) : (
+                        <SelectStyle
+                            value={selectedHeading}
+                            onChange={(event)=> handleHeadingClick(event.target.value)}
+                        >
+                            {Object.keys(privacyNoticeData).map((heading, i) => (
+                                <option key={heading} value={heading}>
+                                    {heading}
+                                </option>
+                            ))}
+                        </SelectStyle>
+                    )}
                     <DetailsPart>
                         {selectedHeading && (
-                            data[selectedHeading]
+                            privacyNoticeData[selectedHeading]
                         )}
                     </DetailsPart>
                 </MainContainer>
+
+                <IconButton
+                    style={{
+                        position: 'absolute',
+                        right: 10,
+                        top: 10
+                    }}
+                    fontSize={'28px'}
+                    bgColor={colors.accent80}
+                    hBgColor={colors.accent100}
+                    aBgColor={colors.accent100}
+                    onClick={()=> navigate(LANDING_PAGE)}
+                >
+                    <BiHomeAlt2/>
+                </IconButton>
             </Wrapper>
-        </BloggiosBase>
     );
 };
 
@@ -76,16 +98,17 @@ const Wrapper = styled.div`
     max-width: 100%;
     display: flex;
     flex-direction: column;
-    height: calc(100vh - 72px);
+    height: 100vh;
     align-items: center;
     justify-content: center;
     box-sizing: border-box;
+    position: relative;
     transition: all 400ms ease-in-out;
 `;
 
 const MainContainer = styled.div`
     width: 70%;
-    height: 95%;
+    height: 90%;
     align-self: center;
     background: ${colors.black400};
     display: flex;
@@ -95,6 +118,21 @@ const MainContainer = styled.div`
     overflow: hidden;
     border-radius: 10px;
     transition: all 400ms ease-in-out;
+    
+    @media (max-width: 1000px) {
+        width: 90%;
+    }
+    
+    @media (max-width: 800px) {
+        width: 95%;
+        height: 80%;
+    }
+    
+    @media (max-width: 600px) {
+        flex-direction: column;
+        padding: 10px;
+        gap: 20px;
+    }
 `;
 
 const HeadingPart = styled.div`
@@ -104,6 +142,10 @@ const HeadingPart = styled.div`
     padding: 6px 0;
     border-right: 1px solid ${colors.white10};
     transition: all 400ms ease-in-out;
+    
+    @media (max-width: 600px) {
+        display: none;
+    }
 `;
 
 const DetailsPart = styled.div`
@@ -112,11 +154,20 @@ const DetailsPart = styled.div`
     overflow: auto;
     padding: 20px;
     transition: all 400ms ease-in-out;
+
+    @media (max-width: 600px) {
+        width: 100%;
+        padding: 10px;
+    }
+    
+    @media (max-width: 400px) {
+        padding: 0;
+    }
 `;
 
 const HeadingCard = styled.div`
     width: 100%;
-    height: 70px;
+    min-height: 70px;
     display: flex;
     align-items: center;
     padding: 0 20px;
@@ -125,7 +176,45 @@ const HeadingCard = styled.div`
     font-family: "Poppins", sans-serif;
     letter-spacing: ${props => props.active ? '2px' : '1px'};
     color: ${props => props.active ? colors.white100 : colors.white70};
+    cursor: pointer;
     transition: all 400ms ease-in-out;
+    
+    @media (max-width: 800px) {
+        padding: 0 10px;
+    }
+`;
+
+const SelectStyle = styled.select`
+    font-family: 'Poppins', sans-serif;
+    display: inline-block;
+    width: 100%;
+    cursor: pointer;
+    outline: 0;
+    border: none;
+    outline: none;
+    background: ${colors.black400};
+    padding: 10px;
+    color: rgba(245, 245, 245, 0.8);
+    border-radius: 4px;
+    font-size: 14px;
+    letter-spacing: 1px;
+    font-weight: 300;
+    border-bottom: 1px solid ${colors.white10};
+
+    &::-ms-expand {
+        display: none;
+    }
+
+    &:hover, &:focus {
+        color: ${colors.white100};
+        background: ${colors.black200};
+        border-bottom: 1px solid ${colors.white10};
+    }
+
+    & option {
+        font-family: 'Poppins', sans-serif;
+        letter-spacing: 1px;
+    }
 `;
 
 export default PrivacyPolicy;
